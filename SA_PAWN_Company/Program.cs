fd376c0e7c31ddc843ef4b8dc2ae28a6c120ae5b
Sambo Chea <sombochea100@gmail.com>
2017-11-26 10:53:25 +0700

    Fi

--- SA_PAWN_Company/Program.cs
+++ SA_PAWN_Company/Program.cs
@@ -17,7 +17,7 @@ namespace SA_PAWN_Company
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmDashboard2());
+            Application.Run(new Login());
         }
     }
 }
